c5fa55580d09d46b6a1500dec613511d1204aa7e
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-15 10:14:30 +0100

    Service & Search modifications

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import {map} from 'rxjs/operators'
+
 @Injectable()
 export class SpotifyService {
 
@@ -15,6 +17,15 @@ export class SpotifyService {
       const headers= new HttpHeaders({
         'Authorization' : 'Bearer BQAEE6CYqq7I-YKb99xTbmdNg72Oilgs0bJ6d23wtZtGb1yY818G7BPBD1Bj0ewB3eEvpnVjPxeGtK5erQUXZMx0HKVx8NFLtp70jUBAPzEFg9Q-n2SOw5pOmLsXx7KSU9ESmIkg5FYsnPLU93dk4dcQa59s3Gg'
       });
-      return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+      return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers}).pipe(map( data=>{
+        return data['albums'].items;
+      }));
+   }
+
+   getArtista(termino:string){
+    const headers= new HttpHeaders({
+      'Authorization' : 'Bearer BQAEE6CYqq7I-YKb99xTbmdNg72Oilgs0bJ6d23wtZtGb1yY818G7BPBD1Bj0ewB3eEvpnVjPxeGtK5erQUXZMx0HKVx8NFLtp70jUBAPzEFg9Q-n2SOw5pOmLsXx7KSU9ESmIkg5FYsnPLU93dk4dcQa59s3Gg'
+    });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers}).pipe(map(data=>data['artists'].items));
    }
 }
