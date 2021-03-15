95285cefa7fae33e0ea6832e910392dc47ff9e32
Salvador Acien Gomez <salvadoraciengomez@protonmail.com>
2021-03-15 14:07:07 +0100

    Updated Service & HomeComponent errMsg

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,13 +11,20 @@ export class HomeComponent implements OnInit {
 
   nuevasCanciones:any[]=[];
   loading:boolean;
+  error:boolean;
+  errMsg:string;
 
   constructor( private spotify:SpotifyService) {
     this.loading=true;
+    this.error=false;
     this.spotify.getNewReleases().subscribe((data:any)=>{
       console.log(data);
       this.nuevasCanciones= data;
       this.loading=false;
+    },(errorSvc)=>{
+      this.loading=false;
+      this.error=true;
+      this.errMsg=errorSvc.error.error.message;
     });
 
   }
